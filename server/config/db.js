7f1fb5a2433787cb0e20dae29d205f07106b0032
mypoliticsweb <73365506+mypoliticsweb@users.noreply.github.com>
2021-03-15 13:37:59 -0400

    blog

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config({ path: "../.env" });
 let MONGOURI = process.env.MONGOURI;
 const InitiateMongoServer = async () => {
+	console.log("mongo:", process.env.MONGOURI);
+	console.log("mongo2:", MONGOURI);
+
 	try {
 		await mongoose.connect(MONGOURI, {
 			useNewUrlParser: true,
