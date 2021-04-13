1c705f06dd49a6ab2cbafa42adc454f4b845b08a
mypoliticsweb <73365506+mypoliticsweb@users.noreply.github.com>
2021-04-13 16:02:14 -0400

    landing page v4

--- server/controllers/blog.js
+++
@@ -1,6 +0,0 @@
-const BlogPost = require("../database/models/BlogPost");
-
-module.exports = async (req, res) => {
-	const blogposts = await BlogPost.find({});
-	res.render("blog", { blogposts });
-};
--- server/controllers/getBlogPost.js
+++
@@ -1,8 +0,0 @@
-const BlogPost = require("../database/models/BlogPost");
-
-module.exports = async (req, res) => {
-	const blogpost = await BlogPost.findById(req.params.id);
-	res.render("blogpost", {
-		blogpost,
-	});
-};
