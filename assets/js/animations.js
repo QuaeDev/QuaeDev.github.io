e1032014f0695f24b7ba9dace0ea652083343fe1
mypoliticsweb <73365506+mypoliticsweb@users.noreply.github.com>
2021-04-14 11:49:29 -0400

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -160,6 +160,7 @@ showSlides();
 function showSlides() {
 	var i;
 	var slides = document.getElementsByClassName("slide-show-card");
+	var dots = document.getElementsByClassName("dot");
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
@@ -167,7 +168,11 @@ function showSlides() {
 	if (slideIndex > slides.length) {
 		slideIndex = 1;
 	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
 	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
 	setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
