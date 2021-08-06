f3c60c71f73f4882959d95701bd9ff3aea08acf6
mypoliticsweb <73365506+QuaeDev@users.noreply.github.com>
2021-08-06 13:33:29 -0400

    in process of componentizing the navbar

--- assets/js/componentRendering.js
+++ assets/js/componentRendering.js
@@ -1,6 +1,11 @@
 $(function () {
-	$("#nav-placeholder").load("../../pages/htmlComponents/nav.html");
+	$(".nav-placeholder").load("../../pages/htmlComponents/nav.html");
 });
 $(function () {
 	$("#footer-placeholder").load("../../pages/htmlComponents/footer.html");
+});
+$(function () {
+	$("#security-svg-placeholder").load(
+		"../../pages/htmlComponents/security_svg.html"
+	);
 });
