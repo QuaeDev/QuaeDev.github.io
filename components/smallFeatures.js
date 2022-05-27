28e5fb2ddfdc3957c22c2986568732c73fc442f2
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-05-27 12:59:51 -0400

    added icons

--- components/smallFeatures.js
+++ components/smallFeatures.js
@@ -1,45 +1,46 @@
 let smallFeatures = [
 	{
-		img: "/assets/img/mission/ballot.png",
-		imgAlt: "blah",
-		text: "blah bah",
+		img: "/assets/img/smallFeatures/security.png",
+		imgAlt: "icon of a lock",
+		text: "Secure Data",
 	},
 	{
-		img: "/assets/img/mission/ballot.png",
-		imgAlt: "blah",
-		text: "blah bah",
+		img: "/assets/img/smallFeatures/supporters.png",
+		imgAlt: "icon of a follow request",
+		text: "Build Support",
 	},
 	{
-		img: "/assets/img/mission/ballot.png",
-		imgAlt: "blah",
-		text: "blah bah",
+		img: "/assets/img/smallFeatures/analytics.png",
+		imgAlt: "icon of analytics",
+		text: "See Analytics",
 	},
 	{
-		img: "/assets/img/mission/ballot.png",
-		imgAlt: "blah",
-		text: "blah bah",
+		img: "/assets/img/smallFeatures/emailDomain.png",
+		imgAlt: "icon of email",
+		text: "Email Domains",
 	},
 	{
-		img: "/assets/img/mission/ballot.png",
-		imgAlt: "blah",
-		text: "blah bah",
+		img: "/assets/img/smallFeatures/accessible.png",
+		imgAlt: "icon of computer, tablet, and phone",
+		text: "Accessible",
 	},
 	{
-		img: "/assets/img/mission/ballot.png",
-		imgAlt: "blah",
-		text: "blah bah",
+		img: "/assets/img/smallFeatures/storage.png",
+		imgAlt: "icon of folder",
+		text: "Record Keeping",
 	},
 ];
 let smallFeatureContainer = document.getElementById("small-features-container");
 for (let feature of smallFeatures) {
 	let card = document.createElement("div");
 	card.classList.add("card");
+	card.classList.add("small-feature");
 	card.innerHTML = /*html*/ `
     <div class="img-container">
         <img src="${feature.img}" alt="${feature.imgAlt}" />
     </div>
     <div class="content">
-        <h4>${feature.text}</h4>
+        <h5>${feature.text}</h5>
     </div>
     `;
 	smallFeatureContainer.appendChild(card);
