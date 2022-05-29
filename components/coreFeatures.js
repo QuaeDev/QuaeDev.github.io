e62a43ab6a3ab373d2aa2b91e80c70eefe50bb8a
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-05-29 12:40:03 -0400

    small changes

--- components/coreFeatures.js
+++ components/coreFeatures.js
@@ -3,38 +3,43 @@ let coreFeatures = [
 		img: "/assets/img/coreFeatures/propose.png",
 		imgAlt: "icon of a proposal",
 		imgClass: "propose",
-		title: "Propose Ideas",
-		body: "this is the body text, blah blah we're gunna put stuff here",
+		title: "Propose",
+		body: "Get involved in the democratic process and write a proposal to address an issue you see.",
 		buttonText: "See More",
-		buttonHandler: "voteHandler()",
+		buttonHandler: "proposeHandler()",
 	},
 	{
 		img: "/assets/img/coreFeatures/vote.png",
 		imgAlt: "icon of someone voting",
 		imgClass: "vote",
-		title: "Vote for Solutions",
-		body: "this is the body text, blah blah we're gunna put stuff here",
+		title: "Vote",
+		body: "Represent your beliefs by voting on proposals in your community",
 		buttonText: "See More",
-		buttonHandler: "proposeHandler()",
+		buttonHandler: "voteHandler()",
 	},
 	{
 		img: "/assets/img/coreFeatures/act.png",
 		imgAlt: "icon of city",
 		imgClass: "act",
-		title: "Enact Change",
-		body: "this is the body text, blah blah we're gunna put stuff here",
+		title: "Act",
+		body: "Verified voters give Quae proposals real power",
 		buttonText: "See More",
 		buttonHandler: "actHandler()",
 	},
 ];
 function voteHandler() {
-	console.log("wow, i pressed a button");
+	// console.log("wow, i pressed a button");
 }
 function proposeHandler() {
-	console.log("wow, i pressed a button");
+	// console.log("wow, i pressed a button");
+	window.open("https://dev.quae.app/post/629112d290661d001669d8b5", "_blank");
 }
 function actHandler() {
-	console.log("wow, i pressed a button");
+	window.open(
+		"https://thestute.com/2021/11/12/student-initiative-to-extend-library-hours-comes-to-fruition-after-quae-poll/",
+		"_blank"
+	);
+	// console.log("wow, i pressed a button");
 }
 let coreFeatureContainer = document.getElementById("core-features-container");
 for (let feature of coreFeatures) {
