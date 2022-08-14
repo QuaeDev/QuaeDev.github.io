2a0ba1e0d1abd4e1168f9b9bc3ea4322a78237d4
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2022-08-14 15:18:59 -0400

    small features

--- components/smallFeatures.js
+++ components/smallFeatures.js
@@ -20,41 +20,38 @@ let smallFeatures = [
 		subText:
 			"Quae's Supporter Network shows true verifiable data about which political personalities represent the most people effectively.",
 	},
-	{
-		img: "/assets/img/smallFeatures/analytics.png",
-		imgAlt: "icon of analytics",
-		text: "See Important Analytics",
-		subText:
-			"Our analytical data shows valuable information about specific groups of people across your community.",
-	},
-	{
-		img: "/assets/img/smallFeatures/emailDomain.png",
-		imgAlt: "icon of email",
-		text: "Require Email Domains",
-		subText:
-			"Protect your community from anyone not within your organization with the use of private domains.",
-	},
+	// {
+	// 	img: "/assets/img/smallFeatures/analytics.png",
+	// 	imgAlt: "icon of analytics",
+	// 	text: "See Important Analytics",
+	// 	subText:
+	// 		"Our analytical data shows valuable information about specific groups of people across your community.",
+	// },
+	// {
+	// 	img: "/assets/img/smallFeatures/emailDomain.png",
+	// 	imgAlt: "icon of email",
+	// 	text: "Require Email Domains",
+	// 	subText:
+	// 		"Protect your community from anyone not within your organization with the use of private domains.",
+	// },
 
-	{
-		img: "/assets/img/smallFeatures/storage.png",
-		imgAlt: "icon of folder",
-		text: "Keep Election Records",
-		subText:
-			"One place to track all of your organizations' important decisions from the time their community was started.",
-	},
+	// {
+	// 	img: "/assets/img/smallFeatures/storage.png",
+	// 	imgAlt: "icon of folder",
+	// 	text: "Keep Election Records",
+	// 	subText:
+	// 		"One place to track all of your organizations' important decisions from the time their community was started.",
+	// },
 ];
 let smallFeatureContainer = document.getElementById("small-features-container");
 for (let feature of smallFeatures) {
 	let card = document.createElement("div");
-	card.classList.add("card");
 	card.classList.add("small-feature");
 	card.innerHTML = /*html*/ `
-    <div class="header">
-        <img src="${feature.img}" alt="${feature.imgAlt}" loading="lazy"/>
-		<h5>${feature.text}</h5>
-    </div>
+	<img src="${feature.img}" alt="${feature.imgAlt}" loading="lazy"/>
     <div class="content">
-		<p class='secondary'>${feature.subText}</p>
+		<h4>${feature.text}</h4>
+		<p>${feature.subText}</p>
     </div>
     `;
 	smallFeatureContainer.appendChild(card);
